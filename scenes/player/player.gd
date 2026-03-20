13bd2fa59f6ae1070d7b6b1dc760957ab8dd02c3
Tornayyydo <sandrosandala22@gmail.com>
2026-03-20 14:55:24 +0400

    horizontal movement implemented

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,26 +1,13 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 200.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(_delta):
-	# Add the gravity.
-	if not is_on_floor():
-		#velocity += get_gravity() * delta
-		pass
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	var direction : float = Input.get_axis("move_left", "move_right")
+	
+	velocity.x = move_speed * direction
+	
 	move_and_slide()
