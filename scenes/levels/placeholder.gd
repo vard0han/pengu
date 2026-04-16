10f2bff3859b0242d1b8e58346f75c5c6b6b9f05
Tornayyydo <sandrosandala22@gmail.com>
2026-04-17 02:51:00 +0400

    [misc] small details
     - moved reticle logic from level to reticle
    - added max fps

--- scenes/levels/placeholder.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var reticle = $Reticle
-
-
-func _process(_delta):
-	reticle.global_position = get_global_mouse_position()
---
+++ scenes/weapons/reticle.gd
@@ -0,0 +1,4 @@
+extends Sprite2D
+
+func _process(_delta) -> void:
+	global_position = get_global_mouse_position()
