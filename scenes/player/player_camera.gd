71ddc81d6cb0116ac7071748fcf029f06b9314cd
Tornayyydo <sandrosandala22@gmail.com>
2026-07-05 04:07:40 +0400

    diverged from true pixel art, cleaned up the camera script

--- scenes/player/player_camera.gd
+++ scenes/player/player_camera.gd
@@ -3,50 +3,50 @@ extends Camera2D
 
 const LOOK_AHEAD_DISTANCE: float = 60.0
 const LOOK_AHEAD_LERP_SPEED: float = 1.5
+const PLAYER_SCREEN_OFFSET: Vector2 = Vector2(0, -25)
 
 var _shake_strength: float = 0.0
 var _shake_duration: float = 0.0
-var _shake_initial_strength: float = 0.0
-var _base_offset: Vector2 = Vector2.ZERO
+var _shake_initial_duration: float = 0.0
+var _shake_offset: Vector2 = Vector2.ZERO
 var _look_ahead_offset: Vector2 = Vector2.ZERO
 
-const PLAYER_SCREEN_OFFSET: Vector2 = Vector2(0, -25)
+var _player: Player = null
 
 func _ready() -> void:
-	_base_offset = offset
+	_player = get_parent() as Player
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	#_update_look_ahead(delta)
 	_update_shake(delta)
-	
-	offset = _base_offset + _look_ahead_offset + PLAYER_SCREEN_OFFSET
+
+	offset = (_look_ahead_offset + PLAYER_SCREEN_OFFSET + _shake_offset).round()
 
 func _update_look_ahead(delta: float) -> void:
-	var player: Player = get_parent() as Player
-	if not player:
+	if not _player:
 		return
-	
+
 	var target_offset: Vector2 = Vector2.ZERO
-	
-	if player.velocity.x > 50.0:
+
+	if _player.velocity.x > 50.0:
 		target_offset.x = LOOK_AHEAD_DISTANCE
-	elif player.velocity.x < -50.0:
+	elif _player.velocity.x < -50.0:
 		target_offset.x = -LOOK_AHEAD_DISTANCE
-	
+
 	_look_ahead_offset = _look_ahead_offset.lerp(target_offset, LOOK_AHEAD_LERP_SPEED * delta)
 
 func _update_shake(delta: float) -> void:
 	if _shake_duration > 0.0:
 		_shake_duration = maxf(_shake_duration - delta, 0.0)
-		var current_strength: float = _shake_strength * (_shake_duration / _shake_initial_strength)
-		_base_offset = Vector2(randf_range(-current_strength, current_strength),randf_range(-current_strength, current_strength))
+		var current_strength: float = _shake_strength * (_shake_duration / _shake_initial_duration)
+		_shake_offset = Vector2(randf_range(-current_strength, current_strength), randf_range(-current_strength, current_strength))
 	else:
-		_base_offset = Vector2.ZERO
+		_shake_offset = Vector2.ZERO
 
 func shake(strength: float, duration: float) -> void:
 	_shake_strength = strength
 	_shake_duration = duration
-	_shake_initial_strength = duration
+	_shake_initial_duration = duration
 
 func shake_small() -> void:
 	shake(1.5, 0.15)
